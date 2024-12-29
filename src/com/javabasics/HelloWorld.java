2e6de9f9e2cfd65fc7478a246a41ef31d17e55ab
Shubham Panigrahi <shubhamin99qa@gmail.com>
2024-12-29 13:27:06 +0530

    Hello World

---
+++ src/com/javabasics/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.javabasics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Print text with new line
+				System.out.println("Hello World");
+				
+				// Print text with new line
+				System.out.print("Hello");
+				System.out.print(" world " );
+				
+				// Print using formatting
+				System.out.printf("Hello,This is %s and this is written %d times.%n","Shubham",5);
+				
+	}
+
+}
