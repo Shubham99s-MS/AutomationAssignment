f3bebbf286444d10757d9cf10a745d33c7da64c4
Shubham Panigrahi <shubhamin99qa@gmail.com>
2025-01-07 20:30:54 +0530

    Assignemnt 8

---
+++ src/com/javabasics/Assignment_7_Employee.java
@@ -0,0 +1,27 @@
+package com.javabasics;
+
+public class Assignment_7_Employee {
+	
+	
+	public class Employee() {
+	    String Name = "Alice Johnson";
+	    int BaseSalary = 75000;
+	    int Experience = 5 ;	 
+	    double yearEndRating= 4.2 ;
+	    
+	    		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class Employee() {
+	    String Name = "Alice Johnson";
+	    int BaseSalary = 75000;
+	    int Experience = 5 ;	 
+	    double yearEndRating= 4.2 ;
+	    
+	    		}
+
+
+	}
+
+}
